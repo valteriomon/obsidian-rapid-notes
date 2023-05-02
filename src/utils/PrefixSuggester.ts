5e0f91a9c5dc239ef26088a98dfe1f9fc7bd31db
valteriomon <wally.pg@outlook.com>
2023-05-02 17:39:47 -0300

    prefix suggest

--- src/utils/PrefixSuggester.ts
+++ src/utils/PrefixSuggester.ts
@@ -1,180 +1,182 @@
+// Credits go to SilentVoid13's Templater Plugin: https://github.com/SilentVoid13/Templater
+
 import {
-    App,
-    normalizePath,
-    Platform,
-    TFolder,
-    Notice,
-    Modal,
-    Instruction,
-  } from 'obsidian';
-  import { NewFileLocation } from './enums';
-  import { path } from './utils';
+  ButtonComponent,
+  Modal,
+  Platform,
+  TextAreaComponent,
+  TextComponent,
+  Instruction,
+} from "obsidian";
+import { FuzzyMatch, FuzzySuggestModal } from "obsidian";
 
-  export default class CreateNoteModal extends Modal {
-    mode: NewFileLocation;
-    folder: TFolder;
-    newDirectoryPath: string;
-    inputEl: HTMLInputElement;
-    instructionsEl: HTMLElement;
-    inputListener: EventListener;
+export interface PrefixFolderTuple {
+  prefix: string;
+  filenamePrefix: string;
+  folder: string;
+  addCommand: boolean;
+}
 
-    constructor(app: App, mode: NewFileLocation) {
+export class PrefixModal extends FuzzySuggestModal<PrefixFolderTuple> {
+  private resolve: (value: T) => void;
+  private reject: () => void;
+  private submitted = false;
+  suggestionEmpty: HTMLDivElement;
+  noSuggestion: boolean;
+
+
+  // inputEl: HTMLInputElement;
+  // instructionsEl: HTMLElement;
+  // inputListener: EventListener;
+
+
+  constructor(
+      private text_items: string[] | ((item: T) => string),
+      private items: T[],
+      placeholder: string,
+      limit?: number
+  ) {
       super(app);
-
-      this.mode = mode;
-
+      this.setPlaceholder(placeholder);
+      limit && (this.limit = limit);
       // create input
-      this.inputEl = document.createElement('input');
-      this.inputEl.type = 'text';
-      this.inputEl.placeholder = 'Type filename for new note';
-      this.inputEl.className = 'prompt-input';
+      // this.inputEl = document.createElement('input');
+      // this.inputEl.type = 'text';
+      // this.inputEl.placeholder = 'New note';
+      // this.inputEl.className = 'prompt-input';
+      // this.instructionsEl = document.createElement('div');
+      // this.instructionsEl.addClass('prompt-instructions');
+      // this.instructionsEl.innerText = "prefixes to save the note in specific folders.";
+      // this.modalEl.className = 'prompt';
+      // this.modalEl.innerHTML = '';
+      // this.modalEl.appendChild(this.inputEl);
+      // this.modalEl.appendChild(this.instructionsEl);
 
-      // create instructions
-      const instructions = [
-        {
-          command: '↵',
-          purpose: 'to create note (default: Untitled)',
-        },
-        {
-          command: 'esc',
-          purpose: 'to dismiss creation',
-        },
-      ] as Instruction[];
-      this.instructionsEl = document.createElement('div');
-      this.instructionsEl.addClass('prompt-instructions');
-      const children = instructions.map((x) => {
-        const child = document.createElement('div');
-        child.addClass('prompt-instruction');
-
-        const command = document.createElement('span');
-        command.addClass('prompt-instruction-command');
-        command.innerText = x.command;
-        child.appendChild(command);
-
-        const purpose = document.createElement('span');
-        purpose.innerText = x.purpose;
-        child.appendChild(purpose);
-
-        return child;
-      });
-      for (const child of children) {
-        this.instructionsEl.appendChild(child);
-      }
-
-      // make modal
-      this.modalEl.className = 'prompt';
-      this.modalEl.innerHTML = '';
-      this.modalEl.appendChild(this.inputEl);
-      this.modalEl.appendChild(this.instructionsEl);
-
-      this.inputListener = this.listenInput.bind(this);
-    }
-
-    setFolder(folder: TFolder, newDirectoryPath: string) {
-      this.folder = folder;
-      this.newDirectoryPath = newDirectoryPath;
-    }
-
-    listenInput(evt: KeyboardEvent) {
-      if (evt.key === 'Enter') {
-        // prevent enter after note creation
-        evt.preventDefault();
-        // Do work
-        this.createNewNote(this.inputEl.value);
-        this.close();
-      }
-    }
-
-    onOpen() {
-      this.inputEl.focus();
-      this.inputEl.addEventListener('keydown', this.inputListener);
-    }
-
-    onClose() {
-      this.inputEl.removeEventListener('keydown', this.inputListener);
-    }
-
-    /**
-     * Creates a directory (recursive) if it does not already exist.
-     * This is a helper function that includes a workaround for a bug in the
-     * Obsidian mobile app.
-     */
-    private async createDirectory(dir: string): Promise<void> {
-      const { vault } = this.app;
-      const { adapter } = vault;
-      const root = vault.getRoot().path;
-      const directoryPath = path.join(this.folder.path, dir);
-      const directoryExists = await adapter.exists(directoryPath);
-      // ===============================================================
-      // -> Desktop App
-      // ===============================================================
-      if (!Platform.isIosApp) {
-        if (!directoryExists) {
-          return adapter.mkdir(normalizePath(directoryPath));
-        }
-      }
-      // ===============================================================
-      // -> Mobile App (IOS)
-      // ===============================================================
-      // This is a workaround for a bug in the mobile app:
-      // To get the file explorer view to update correctly, we have to create
-      // each directory in the path one at time.
-
-      // Split the path into an array of sub paths
-      // Note: `normalizePath` converts path separators to '/' on all platforms
-      // @example '/one/two/three/' ==> ['one', 'one/two', 'one/two/three']
-      // @example 'one\two\three' ==> ['one', 'one/two', 'one/two/three']
-      const subPaths: string[] = normalizePath(directoryPath)
-        .split('/')
-        .filter((part) => part.trim() !== '')
-        .map((_, index, arr) => arr.slice(0, index + 1).join('/'));
-
-      // Create each directory if it does not exist
-      for (const subPath of subPaths) {
-        const directoryExists = await adapter.exists(path.join(root, subPath));
-        if (!directoryExists) {
-          await adapter.mkdir(path.join(root, subPath));
-        }
-      }
-    }
-
-    /**
-     * Handles creating the new note
-     * A new markdown file will be created at the given file path (`input`)
-     * in the specified parent folder (`this.folder`)
-     */
-    async createNewNote(input: string): Promise<void> {
-      const { vault } = this.app;
-      const { adapter } = vault;
-      const prependDirInput = path.join(this.newDirectoryPath, input);
-      const { dir, name } = path.parse(prependDirInput);
-      const directoryPath = path.join(this.folder.path, dir);
-      const filePath = path.join(directoryPath, `${name}.md`);
-
-      try {
-        const fileExists = await adapter.exists(filePath);
-        if (fileExists) {
-          // If the file already exists, respond with error
-          throw new Error(`${filePath} already exists`);
-        }
-        if (dir !== '') {
-          // If `input` includes a directory part, create it
-          await this.createDirectory(dir);
-        }
-        const File = await vault.create(filePath, '');
-        // Create the file and open it in the active leaf
-        let leaf = this.app.workspace.getLeaf(false);
-        if (this.mode === NewFileLocation.NewPane) {
-          leaf = this.app.workspace.splitLeafOrActive();
-        } else if (this.mode === NewFileLocation.NewTab) {
-          leaf = this.app.workspace.getLeaf(true);
-        } else if (!leaf) {
-          // default for active pane
-          leaf = this.app.workspace.getLeaf(true);
-        }
-        await leaf.openFile(File);
-      } catch (error) {
-        new Notice(error.toString());
-      }
-    }
+      // this.inputListener = this.listenInput.bind(this);
   }
+
+  renderSuggestion(match: FuzzyMatch<PrefixFolderTuple>, el: HTMLElement) {
+
+      const suggestion = `${match.item.prefix}: ${match.item.folder}`
+      el.setText(suggestion);
+  }
+
+  onNoSuggestion() {
+      // this.emptyStateText = "AAA";
+      this.noSuggestion = true;
+      // this.newDirectoryPath = this.inputEl.value;
+      this.resultContainerEl.innerHTML = '';
+      // this.resultContainerEl.childNodes.forEach((c) =>
+
+      //     c.parentNode.removeChild(c)
+      // );
+      // this.chooseFolder.innerText = this.inputEl.value;
+      // this.resultContainerEl.appendChild(this.chooseFolder);
+      this.suggestionEmpty = document.createElement('div');
+      this.suggestionEmpty.addClass('suggestion-empty');
+      this.suggestionEmpty.innerText = "EMPTY_TEXT";
+      this.resultContainerEl.appendChild(this.suggestionEmpty);
+  }
+
+  // listenInput(evt: KeyboardEvent) {
+  //     if (evt.key === 'Enter') {
+  //         // prevent enter after note creation
+  //         evt.preventDefault();
+  //         this.enterCallback(evt);
+  //     }
+  // }
+
+  // onOpen(): void {
+  //     this.inputEl.focus();
+  //     this.inputEl.addEventListener('keydown', this.inputListener);
+  // }
+
+  // onClose(): void {
+  //     this.inputEl.removeEventListener('keydown', this.inputListener);
+  //     this.contentEl.empty();
+  //     if (!this.submitted) {
+  //         // TOFIX: for some reason throwing Error on iOS causes the app to freeze.
+  //         this.reject();
+  //     }
+  // }
+
+  // private enterCallback(evt: KeyboardEvent) {
+  //     if (evt.key === "Enter") {
+  //         this.resolveAndClose(evt);
+  //     }
+  // }
+
+  // private resolveAndClose(evt: Event | KeyboardEvent) {
+  //     this.submitted = true;
+  //     evt.preventDefault();
+  //     this.resolve(this.inputEl.value);
+  //     this.close();
+  // }
+  getItems(): T[] {
+      return this.items;
+  }
+
+  onClose(): void {
+      if (!this.submitted) {
+          this.reject();
+      }
+  }
+
+  // renderSuggestion() {
+
+  // }
+
+  onChooseSuggestion(item: any, evt: any) {
+      console.log(item)
+  }
+
+  selectSuggestion(
+      value: FuzzyMatch<T>,
+      evt: MouseEvent | KeyboardEvent
+  ): void {
+      // this.submitted = true;
+      // this.close();
+      console.log(value.item, typeof value.item)
+      if(typeof value.item === "string") {
+          this.inputEl.value = value.item + " ";
+      }
+
+      this.onChooseSuggestion(value, evt);
+  }
+
+  // getItemValue(item: T): string {
+  //     return item.item;
+  //     if (this.text_items instanceof Function) {
+  //         return this.text_items(item);
+  //     }
+  //     return (
+  //         this.text_items[this.items.indexOf(item)] || "Undefined Text Item"
+  //     );
+  // }
+
+  getItemText(item: T): string {
+      // console.log("item", item)
+      if (this.text_items instanceof Function) {
+          return this.text_items(item);
+      }
+      // return item;
+      return (
+          this.text_items[this.items.indexOf(item)] || "Undefined Text Item"
+      );
+  }
+
+  onChooseItem(item: T): void {
+      console.log(item)
+      // this.resolve(item);
+  }
+
+  async openAndGetValue(
+      resolve: (value: T) => void,
+      reject: () => void
+  ): Promise<void> {
+      this.resolve = resolve;
+      this.reject = reject;
+      this.open();
+  }
+
+}
